bff3546cb71393a4b9e8cd4dc0294556e3273caf
kumar <kumarsanjoy.shuvo@gmail.com>
2025-09-17 13:22:22 +0600

    morgan logger added

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 const port = 3000;
+
+// HTTP request logging
+app.use(morgan('tiny')); // minimal format
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.get('/owner', (req, res) => { 
-    let num = 0;
-    for(let i=0; i<100000000; i++) {
-        num ++;
-    }  
-    res.send(`Owned by Sanjoy. Version: ${num}`);
+  let num = 0;
+  for (let i = 0; i < 100000000; i++) {
+    num++;
+  }
+  res.send(`Owned by Sanjoy. Version: ${num}`);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
